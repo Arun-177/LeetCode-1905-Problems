f4a972622349c07ed61ab7ad13e967d71b95f8d3
Arun-177 <44317942+Arun-177@users.noreply.github.com>
2021-06-25 19:02:22 +0530

    Create 3._Longest_Substring_Without_Repeating_Characters.cpp

---
+++ 3._Longest_Substring_Without_Repeating_Characters.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int op=0,i=0,j;
+        vector<int> lastseen(256,-1);
+        for(j=0;j<s.size();j++){
+            i = max(i,lastseen[s[j]]+1);
+            op = max(op,j-i+1);
+            lastseen[s[j]]=j;
+        }
+        return op;
+        
+        
+    }
+};
